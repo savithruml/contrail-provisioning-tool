4a29977694a9fd4d6642f16e5a7aa977a1b06f42
root <root@netman.(none)>
2017-05-17 11:20:30 -0600

    Removing IP address

--- css-test/launch.py
+++ css-test/launch.py
@@ -1,5 +1,7 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
-import os
+from flask import Flask, flash, redirect, render_template, request, session, abort, Response
+import os, subprocess
+from shelljob import proc
+
 
 application = Flask(__name__)
 
@@ -9,26 +11,29 @@ def provision():
 	target_node_ip = str(request.form['target-ip'])
 	target_node_password = str(request.form['target-password'])
 	base_image = str(request.form['image'])
-	contrail_package_version = str(request.form['package'])
+	contrail_package_version = str(request.form['package']).split('contrail-')[1]
 	contrail_package_release = str(request.form['release'])
 	contrail_package_build = str(request.form['build'])
 	testbed_file = request.files['testbed-file'].read()
-	
-	print "\n\n\n"
-	print contrail_package_build
 
-	localfile = open("testbed-example.py", "w")
+	localfile = open("testbed.py", "w")
 	localfile.write(testbed_file)
 	localfile.close()
+	
+	if request.form["commit"] == "Provision":
+		def generate():
+			for line in (subprocess.check_output("ssh root@{4} wget http://10.84.5.120/github-build/R{0}/{1}/{2}/{3}/artifacts/contrail-cloud-docker_4.0.0.0-{1}-{3}.tgz && \
+		         ssh root@{4} wget http://10.84.5.120/github-build/R{0}/{1}/{2}/{3}/artifacts/contrail-server-manager-installer_4.0.0.0-{1}~{3}_all.deb && \
+		         ssh root@{4} truncate -s 0 /etc/apt/sources.list && \
+		         scp testbed.py root@{4}:~/ && \
+		         ssh root@{4} dpkg -i contrail-server-manager*deb". \
+		         format(contrail_package_version,contrail_package_build,base_image,contrail_package_release,target_node_ip), shell=True)).splitlines():
+					 yield line + '\n'
+					 
+		os.remove("testbed.py")
+		
+		return Response( generate(), mimetype= 'text/html' )
 
-	if request.form['btn'] == "reimage":
-		print "Reimage Button"
-	elif request.form['btn'] == "Provision":
-		print "Provision Button"
-	elif request.form['btn'] == "exit":
-		return render_template('login.html')
-	else:
-		print "None Selected"
 
 @application.route('/login', methods=['POST', 'GET'])
 def login():
